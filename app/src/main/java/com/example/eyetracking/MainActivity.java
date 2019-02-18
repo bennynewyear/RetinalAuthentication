5b9027de9c1d231a14fffe57a20960c52824b6e2
jhony-alavez <jia2707@rit.edu>
2019-02-18 15:45:18 -0500

    Commit 001 - EYETRACKING BUTTON INSTANCE.FIXED OPENCV "GL" Errors.

--- app/src/main/java/com/example/eyetracking/MainActivity.java
+++ app/src/main/java/com/example/eyetracking/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.eyetracking;
 
+import android.content.Intent;
+import android.hardware.camera2.CaptureRequest;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivity(intent);
+            }
+        });
     }
+
 }
